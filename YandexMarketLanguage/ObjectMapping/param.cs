8c9a0b192fa8d3cbc34a73829fba9e6b9de59aff
Saito Nakamura <saitonakamuramb@gmail.com>
2016-03-21 15:41:53 +0300

    delete finished todo

--- YandexMarketLanguage/ObjectMapping/param.cs
+++ YandexMarketLanguage/ObjectMapping/param.cs
@@ -7,7 +7,6 @@ namespace YandexMarketLanguage.ObjectMapping
     ///     Goods specification
     /// </summary>
     [Serializable]
-    // TODO tests for param
     public class param
     {
         private string _name;
